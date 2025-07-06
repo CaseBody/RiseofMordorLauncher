37db3d67d8c041dc1d9fcdfbc199bcea224a1c84
victimized_pc <salimovhayali@gmail.com>
2025-07-06 12:21:03 +0200

    fixes

--- RiseofMordorLauncher/Directory/ViewModels/MainLauncherViewModel.cs
+++ RiseofMordorLauncher/Directory/ViewModels/MainLauncherViewModel.cs
@@ -13,6 +13,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
